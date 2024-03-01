88a34f454a99ac96e93a93453f4b473559c6e2f9
Leon Clarkson <le.clarkson@roncalli.school.nz>
2024-03-01 17:46:44 +1300

    leon progress

---
+++ ShoeCard.js
@@ -0,0 +1,10 @@
+
+
+const ShoeCard = () => {
+    <div>
+      <img style="height: 240px; width: 240px" src="download.jpg" />
+      <p style="color: bisque">Campus</p>
+      <p style="color: bisque">Adidas</p>
+      <p style="color: bisque">$190</p>
+    </div>
+  }
